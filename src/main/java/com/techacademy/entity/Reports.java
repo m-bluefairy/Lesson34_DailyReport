cc51cbf6dda91e8192ddc3c285d1343643152088
m-bluefairy <awim@me.com>
2024-10-20 17:29:57 +0900

    1020_修正版

--- src/main/java/com/techacademy/entity/Reports.java
+++ src/main/java/com/techacademy/entity/Reports.java
@@ -2,7 +2,7 @@ package com.techacademy.entity;
 
 import java.time.LocalDateTime;
 import java.time.LocalDate;
-import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
@@ -15,7 +15,7 @@ public class Reports {
     public static enum Role {
         GENERAL("一般"), ADMIN("管理者");
 
-    	private String value;
+        private String value;
 
         private Role(String value) {
             this.value = value;
@@ -26,31 +26,31 @@ public class Reports {
         }
     }
 
- // ID
+    // ID
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     // 日付
-    @Column(columnDefinition="DATE", nullable = false)
+    @Column(columnDefinition = "DATE", nullable = false)
     private LocalDate reportDate;
 
     // タイトル
-    @Column(columnDefinition="VARCHAR(100)", nullable = false)
+    @Column(columnDefinition = "VARCHAR(100)", nullable = false)
     private String title;
 
-    //内容
-    @Column(columnDefinition="LONGTEXT", nullable = false)
+    // 内容
+    @Column(columnDefinition = "LONGTEXT", nullable = false)
     @NotEmpty
     private String content;
 
-    // 社員番号
+    // 社員情報
     @ManyToOne
     @JoinColumn(name = "employee_code", referencedColumnName = "code", nullable = false)
     private Employee employee;
 
     // 削除フラグ(論理削除を行うため)
-    @Column(columnDefinition="TINYINT", nullable = false)
+    @Column(columnDefinition = "TINYINT", nullable = false)
     private boolean deleteFlg;
 
     // 登録日時
